0eaf9e4816c27a8c5cae1788e1ec302e3bbb94bc
fitodac <fitodac@gmail.com>
2023-05-15 05:28:54 -0300

    Home page hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,12 +1,12 @@
 <script setup>
-const button_class = 'text-white text-center font-medium px-8 py-2.5 flex-1 rounded-lg transition-all hover:opacity-80'
+const button_class = 'text-white text-center font-medium px-8 py-4 flex-1 rounded-lg transition-all hover:opacity-80'
 </script>
 
 
 <template>
-<section class="px-6 bg-no-repeat bg-left bg-cover md:bg-[url('/img/hero-lg.webp')] 2xl:px-0">
+<section class="px-6 bg-no-repeat bg-left bg-cover md:bg-[url('/img/hero-lg.webp')] xl:bg-[#100023] 2xl:px-0 2xl:bg-contain 2xl:bg-right">
 	<div class="container mx-auto">
-		<div class="md:w-1/2 md:py-20 lg:w-6/12 lg:py-28 xl:w-5/12">
+		<div class="md:w-1/2 md:py-20 lg:w-6/12 lg:py-28 xl:w-5/12 xl:py-36">
 
 			<img 
 				src="/img/hero-sm.webp" 
@@ -16,9 +16,9 @@ const button_class = 'text-white text-center font-medium px-8 py-2.5 flex-1 roun
 			<img 
 				src="/img/brand-white.svg" 
 				alt="Esteve SNC"
-				class="w-40 hidden md:block">
+				class="w-40 hidden md:block lg:w-48">
 			
-			<h2 class="text-5xl font-bold leading-none mt-4 select-none md:mt-5">
+			<h2 class="text-5xl font-bold leading-none mt-4 select-none md:mt-5 xl:text-6xl">
 				<span class="text-aqua block">Porque dos cabezas</span>
 				<span class="text-purple block">piensan más que una,</span>
 			</h2>
@@ -27,7 +27,7 @@ const button_class = 'text-white text-center font-medium px-8 py-2.5 flex-1 roun
 				<strong>ESTEVE</strong> está siempre contigo
 			</h3>
 
-			<div class="mt-7 flex gap-x-5">
+			<div class="mt-10 flex gap-x-5 max-w-md lg:mt-14">
 				<NuxtLink 
 					href="/registro"
 					class="bg-aqua"
@@ -43,6 +43,16 @@ const button_class = 'text-white text-center font-medium px-8 py-2.5 flex-1 roun
 				</NuxtLink>
 			</div>
 
+		</div>
+	</div>
+</section>
+
+<section class="px-6 mt-12 md:-mt-10 lg:-mt-16 2xl:px-0">
+	<div class="container mx-auto">
+		<div class="bg-purple text-white text-xl text-center max-w-md px-14 py-10 mx-auto rounded-2xl md:max-w-full md:text-2xl md:py-16 md:px-32">
+			<p>
+				Descubre aquí nuevos <strong>conocimientos</strong>, <strong>recursos</strong> útiles para tu práctica diaria e <strong>información de tratamientos</strong> para que nunca dejes de avanzar en el cuidado de las personas.
+			</p>
 		</div>
 	</div>
 </section>
