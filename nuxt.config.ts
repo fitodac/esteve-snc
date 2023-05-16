9647e374410c88e94b35013a9e32fb239ffb2c9b
fitodac <fitodac@gmail.com>
2023-05-16 05:56:02 -0300

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	runtimeConfig: {
+    public: {
+      API_URL: 'https://canalestevesnc.com/api/wp-json/wp/v2',
+    }
+  },
+
 	css: [
 		'remixicon/fonts/remixicon.css',
 		'@/css/header.scss',
-		'@/css/main.css',
+		'@/css/main.scss',
 	],
 
 	modules: [
