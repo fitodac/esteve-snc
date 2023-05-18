087acca0f9b1af13df6cf3079df72a7dca56dc81
fitodac <fitodac@gmail.com>
2023-05-18 08:39:37 -0300

    carousel components

---
+++ components/carousels/Carousel4.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Slider from './CarouselLayout'
+
+defineProps({ data: Array })
+</script>
+
+
+<template>
+	<div class="">
+		<Slider :data="data">
+			<swiper-slide 
+				v-for="p in data" 
+				:key="Math.floor(Math.random() * 1000000).toString()">
+
+				<div class="bg-slate-100 w-full h-32 rounded-lg"></div>
+				<h3 class="text-lg font-medium leading-tight mt-3" v-html="p.title" />
+				<div class="mt-6">
+					<nuxt-link 
+						:to="p.file.url" 
+						target="_blank"
+						class="inline-flex transition-all hover:opacity-80">
+						<img src="/img/icon-download.svg" alt="Icono de descarga" class="w-8 h-8">
+					</nuxt-link>
+				</div>
+
+			</swiper-slide>
+		</Slider>
+	</div>
+</template>
---
+++ components/carousels/CarouselLayout.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { onMounted } from 'vue'
+import { register } from 'swiper/element/bundle'
+
+register()
+
+const props = defineProps({ data: Array })
+
+// console.log('data', props.data)
+
+const slider_id = `slider-${Math.floor(Math.random() * 1000000).toString()}`
+
+onMounted(() => {
+	const slider = document.getElementById(slider_id)
+	console.log('slider', slider)
+
+	Object.assign(slider, {
+		slidesPerView: 1,
+		breakpoints: {
+			620: {
+				slidesPerView: 2
+			},
+			700: {
+				slidesPerView: 3,
+				centeredSlides: props.data.length > 2 ? true : false
+			}
+		},
+		spaceBetween: 20,
+	})
+
+	slider.initialize()
+})
+</script>
+
+
+<template>
+	<swiper-container 
+		init="false" 
+		:id="slider_id"
+		class="mx-6 mt-6">
+		<slot />
+	</swiper-container>
+</template>
