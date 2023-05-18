087d8e7b612a0cd2a9b7b71f116a98ab1e4b2e47
fitodac <fitodac@gmail.com>
2023-05-18 09:32:50 -0300

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,33 @@ export default defineNuxtConfig({
     }
   },
 
+	app: {
+		head: {
+			title: 'Canal Esteve SNC',
+			link: [
+				{
+					rel: 'apple-touch-icon',
+					sizes: '180x180',
+					href: '/apple-touch-icon.png'
+				},
+				{
+					rel: 'icon',
+					sizes: '32x32',
+					href: '/favicon-32x32.png'
+				},
+				{
+					rel: 'icon',
+					sizes: '16x16',
+					href: '/favicon-16x16.png'
+				},
+				{
+					rel: 'manifest',
+					href: '/site.webmanifest'
+				}
+			]
+		},
+	},
+
 	css: [
 		'remixicon/fonts/remixicon.css',
 		'@/css/header.scss',
