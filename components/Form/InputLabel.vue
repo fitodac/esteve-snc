1a37983e164a6863340d5f0e2b15ee7c0a969bfa
fitodac <fitodac@gmail.com>
2023-05-31 05:03:19 -0300

    form

---
+++ components/Form/InputLabel.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['value']);
+</script>
+
+<template>
+	<label class="block font-medium text-sm text-fuchsia-800">
+		<span v-if="value">{{ value }}</span>
+		<span v-else><slot /></span>
+	</label>
+</template>
+
+
+<style scoped>
+label{
+	@apply text-fuchsia-800 leading-none font-bold;
+}
+</style>
